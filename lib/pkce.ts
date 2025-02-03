e776488d03b6b7668fb9fcd4052f2d14506904d3
sundaray <pgcim14.hemant@spjimr.org>
2025-02-03 11:42:40 +0530

    create the generateCodeChallenge function

--- lib/pkce.ts
+++ lib/pkce.ts
@@ -1,7 +1,16 @@
 import { base64url } from "jose";
 
-export function generateCodeVerifier() {
+export function generateCodeVerifier(): string {
   const randomValues = new Uint8Array(32);
   crypto.getRandomValues(randomValues);
   return base64url.encode(randomValues);
+}
+
+export async function generateS256CodeChallenge(
+  codeverifier: string,
+): Promise<string> {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeverifier);
+  const hash = await crypto.subtle.digest("SHA256", data);
+  return base64url.encode(new Uint8Array(hash));
 }
