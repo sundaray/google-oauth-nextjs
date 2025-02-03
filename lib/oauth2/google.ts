b79b7591a35bc65b44819f371b93999744e41f65
sundaray <pgcim14.hemant@spjimr.org>
2025-02-03 23:24:57 +0530

    fixed google exxports

--- lib/oauth2/google.ts
+++ lib/oauth2/google.ts
@@ -4,71 +4,59 @@ import { encodeBasicCredentials } from "@/lib/oauth2/utils";
 const authorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 const tokenEndpoint = "https://oauth2.googleapis.com/token";
 
-export class Google {
-  public clientId: string;
+export async function createAuthorizationURLWithPKCE(
+  state: string,
+  clientId: string,
+  redirectURI: string,
+  codeVerifier: string,
+  scopes: string[],
+) {
+  const codeChallenge = await generateS256CodeChallenge(codeVerifier);
 
-  private clientPassword: string;
-  private redirectURI: string;
-
-  constructor(clientId: string, clientPassword: string, redirectURI: string) {
-    this.clientId = clientId;
-    this.clientPassword = clientPassword;
-    this.redirectURI = redirectURI;
+  const url = new URL(authorizationEndpoint);
+  url.searchParams.set("response_type", "code");
+  url.searchParams.set("client_id", clientId);
+  url.searchParams.set("redirect_uri", redirectURI);
+  url.searchParams.set("state", state);
+  url.searchParams.set("code_challenge", codeChallenge);
+  url.searchParams.set("code_challenge_method", "S256");
+  url.searchParams.set("prompt", "select_account");
+  if (scopes.length > 0) {
+    url.searchParams.set("scope", scopes.join(" "));
   }
+  return url;
+}
 
-  public async createAuthorizationURLWithPKCE(
-    state: string,
-    codeVerifier: string,
-    prompt: boolean,
-    scopes: string[],
-  ) {
-    const url = new URL(authorizationEndpoint);
-    url.searchParams.set("response_type", "code");
-    url.searchParams.set("client_id", this.clientId);
-    if (this.redirectURI !== null) {
-      url.searchParams.set("redirect_uri", this.redirectURI);
-    }
-    url.searchParams.set("state", state);
-    const codeChallenge = await generateS256CodeChallenge(codeVerifier);
-    url.searchParams.set("code_challenge_method", "S256");
-    url.searchParams.set("code_challenge", codeChallenge);
-    if (scopes.length > 0) {
-      url.searchParams.set("scope", scopes.join(" "));
-    }
-    if (prompt) {
-      url.searchParams.set("prompt", "select_account");
-    }
-    return url;
-  }
+export async function exchangeCodeForTokens(
+  code: string,
+  codeVerifier: string,
+  clientId: string,
+  clientPassword: string,
+  redirectURI: string,
+) {
+  const encodedCredentials = encodeBasicCredentials(clientId, clientPassword);
 
-  public async exchangeCodeForTokens(code: string, codeVerifier: string) {
-    const encodedCredentials = encodeBasicCredentials(
-      this.clientId,
-      this.clientPassword,
-    );
+  const response = await fetch(tokenEndpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json",
+      Authorization: `Basic ${encodedCredentials}`,
+    },
+    body: new URLSearchParams({
+      code,
+      client_id: clientId,
+      code_verifier: codeVerifier,
+      redirect_uri: redirectURI,
+      grant_type: "authorization_code",
+    }),
+  });
 
-    const response = await fetch(tokenEndpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Accept: "application/json",
-        Authorization: `Basic ${encodedCredentials}`,
-      },
-      body: new URLSearchParams({
-        code,
-        code_verifier: codeVerifier,
-        grant_type: "authorization_code",
-        client_id: this.clientId,
-        redirect_uri: this.redirectURI,
-      }),
-    });
-
-    if (response.status === 400) {
-      return null;
-    }
-
+  if (!response.ok) {
     const data = await response.json();
-
-    return {};
+    console.log("Access Token Response Error: ", data);
   }
+
+  const data = await response.json();
+  console.log("Access Token response: ", data);
 }
